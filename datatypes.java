dcd000321301ff49e45b46a53bb07a40534f632c
Pendem Manideep <103667744+manideep088@users.noreply.github.com>
2023-03-28 01:28:23 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,19 @@
+public class datatypes {
+    public static void main(String args[]) {
+        int marks=100;  //4 Bytes
+        float percentage=8.9f; 
+        double price=89.9887;
+        long distance=93765492;
+        short count=4;
+        char r='K';
+        boolean tiger=true;
+        System.out.println(marks);
+        System.out.println(percentage);
+        System.out.println(r);
+        System.out.println(tiger);
+        System.out.println(price);
+        System.out.println(distance);
+        System.out.println(count);
+    }
+    
+}
---
+++ variable1.java
@@ -0,0 +1,9 @@
+public class variable1 {
+    public static void main(String args[]) {
+        int num1=30;
+        int num2=80;
+        int result= num1*num2;
+        System.out.println(result);
+        
+    }
+}
